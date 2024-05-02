7545501b350ab24e041f5dba46e3f3c312d16dc1
Argenis De La Rosa <theonlyhennygod@gmail.com>
2024-05-02 16:22:27 -0400

    fixed the header

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomeScreen from './screens/home/HomeScreen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div data-theme="dark">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div data-theme="winter" className='flex flex-col items-center p-4 md:p-10'>
+      <div className='max-w-2xl w-full items-center bg-red-100'>
+        <HomeScreen />
+      </div>
     </div>
   )
 }
