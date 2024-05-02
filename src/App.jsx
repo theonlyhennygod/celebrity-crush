e108339756f3e89efba09209bd0556e5fd9d1470
Argenis De La Rosa <theonlyhennygod@gmail.com>
2024-05-02 19:35:26 -0400

    added browser router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import HomeScreen from './screens/home/HomeScreen'
 import { ThemeContext } from './context/ThemeContext'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import AddNewScreen from './screens/NewIdea/AddNewScreen'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen />,
+  },
+  {
+    path: "/new",
+    element: <AddNewScreen />,
+]);
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +25,7 @@ function App() {
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div data-theme={theme} className='flex flex-col items-center p-4 md:p-10'>
         <div className='max-w-2xl w-full items-center'>
-          <HomeScreen />
+          <RouterProvider router={router}>
         </div>
       </div>
     </ThemeContext.Provider>
---
+++ src/screens/NewIdea/AddNewScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddNewScreen() {
+  return (
+    <div>
+      <h1>Add New Screen</h1>
+    </div>
+  )
+}
